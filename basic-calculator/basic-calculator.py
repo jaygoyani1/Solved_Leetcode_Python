cf09bfbf7dc898cc786dfa654abcb35c0afb949b
Jay Goyani <goyanijay@gmail.com>
2021-04-13 17:13:41 -0400

    Time: 116 ms (35.21%), Space: 15.7 MB (63.46%) - LeetHub

--- basic-calculator/basic-calculator.py
+++ basic-calculator/basic-calculator.py
@@ -1,20 +1,42 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stk = []
-        curr = 0
+        total = 0
+        i = 0
+        sign = [1,1]
         
-        for i in "(" + s + ")":
-            if i.isdigit():
-                curr = curr*10 + int(i)
-            elif i == "(":
-                stk += [0,'+']
-                curr = 0
-            elif i != " ":
-                operator, prev = stk.pop(), stk.pop()
-                curr = prev + (curr if operator == "+" else -curr)
-                if i == ")":
-                    continue
-                stk += [curr, i]
-                curr = 0
-        return curr
+        while i<len(s):
+            c = s[i]
+            if c.isdigit():
+                start = i
+                while i <len(s) and s[i].isdigit():
+                    i+=1
+                total += sign.pop()*int(s[start:i])
+                continue
+            if c in "(+-":
+                if c == "-":
+                    sign.append(sign[-1]*(-1))
+                else:
+                    sign.append(sign[-1])
+            elif c == ")":
+                sign.pop()
+            i+=1
+        return total
+#         stk = []
+#         curr = 0
+        
+#         for i in "(" + s + ")":
+#             if i.isdigit():
+#                 curr = curr*10 + int(i)
+#             elif i == "(":
+#                 stk += [0,'+']
+#                 curr = 0
+#             elif i != " ":
+#                 operator, prev = stk.pop(), stk.pop()
+#                 curr = prev + (curr if operator == "+" else -curr)
+#                 if i == ")":
+#                     continue
+#                 stk += [curr, i]
+#                 curr = 0
+#         return curr
+
                 
