f1a0f0392ffe0c90a372f9c1a003310ccba2c297
Jay Goyani <goyanijay@gmail.com>
2021-04-28 20:10:46 -0400

    Time: 3832 ms (59.15%), Space: 14 MB (99.84%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,21 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        lenx, leny = len(board), len(board[0])
-        def dfs(r,c,wordi):
-            if wordi == len(word):
-                return True
-            if r<0 or r>=lenx or c<0 or c>=leny or board[r][c] != word[wordi]:
+        def backtrack(i,j,index):
+            if board[i][j] != word[index]:
                 return False
-            temp = board[r][c]
-            board[r][c] = "*"
-            x = dfs(r+1,c,wordi+1) or dfs(r-1,c,wordi+1) or dfs(r,c+1,wordi+1) or dfs(r,c-1,wordi+1)
-            board[r][c] = temp
-            return x
-        
-        for i in range(lenx):
-            for j in range(leny):
-                if board[i][j] == word[0]:
-                    if dfs(i,j,0):
+            if index == len(word)-1:
+                return True
+            x = board[i][j]
+            board[i][j] = "*"
+            for r,c in [(-1,0),(1,0),(0,-1),(0,1)]:
+                rx = r + i
+                cx = c + j
+                if 0<=rx< len(board) and 0<=cx<len(board[0]):
+                    if backtrack(rx,cx,index+1):
                         return True
+            board[i][j] = x
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtrack(i,j,0):
+                    return True
         return False
         
