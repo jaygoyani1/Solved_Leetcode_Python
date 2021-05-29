1ddd18d28bf150cfc97dff566dd3ff0d0b447537
Jay Goyani <goyanijay@gmail.com>
2021-05-29 10:09:01 -0400

    Time: 40 ms (10.25%), Space: 14.3 MB (33.72%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,24 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stk = []
-        for i in s:
-            if i == "(" or i == "{" or i =="[":
-                stk.append(i)
-            else:
-                if not stk:
-                    return False
-                else:
-                    x = stk.pop()
-                    if i == ")":
-                        if x != "(":
-                            return False
-                    elif i == "]":
-                        if x != "[":
-                            return False
-                    else:
-                        if x != "{":
-                            return False
-        if stk:
-            return False
-        return True
         
+        mapping = {"(": ")", "{": "}", "[": "]"}
+        
+        stack = []
+        
+        for char in s:
+            if char in mapping:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if mapping[stack.pop()] != char:
+                    return False
+        return len(stack) == 0
+            
