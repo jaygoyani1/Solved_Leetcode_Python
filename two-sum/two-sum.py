354f6c902e3fec3182f7026d40c31f4248647ef7
Jay Goyani <goyanijay@gmail.com>
2021-05-29 09:22:01 -0400

    Time: 60 ms (22.25%), Space: 15.3 MB (11.86%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,10 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic = {}
-        for i in range(len(nums)):
-            t = target - nums[i]
-            if t in dic:
-                return [i,dic[t]]
-            dic[nums[i]] = i
         
+        dic = {}
+        
+        for i in range(len(nums)):
+            remaining = target - nums[i]
+            if remaining in dic:
+                return [i,dic[remaining]]
+            else:
+                dic[nums[i]] = i
+        
+            
+            
+
         
