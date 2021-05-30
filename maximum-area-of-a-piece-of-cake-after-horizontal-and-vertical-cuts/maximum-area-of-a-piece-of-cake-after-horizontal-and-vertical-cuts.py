98e13acfb58c7359b9277b406681174446a064e8
Jay Goyani <goyanijay@gmail.com>
2021-05-30 12:33:09 -0400

    Time: 316 ms (40.51%), Space: 27.2 MB (45.88%) - LeetHub

--- maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,14 +1,17 @@
 class Solution:
-    def maxArea(self, h: int, w: int, hori: List[int], verti: List[int]) -> int:
-        hori.sort()
-        verti.sort()
-        x = hori[0]
-        for i in range(1, len(hori)):
-            x = max(x, hori[i]-hori[i-1])
-        x = max(x, h - hori[-1])
-        y = verti[0]
-        for i in range(1, len(verti)):
-            y = max(y, verti[i]-verti[i-1])
-        y = max(y, w - verti[-1])
-        return (x*y)%1000000007
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
         
+        horizontalCuts.sort()
+        verticalCuts.sort()
+        
+        maxh = max(horizontalCuts[0], h-horizontalCuts[-1])
+        maxv = max(verticalCuts[0], w-verticalCuts[-1])
+        
+        for i in range(1,len(horizontalCuts)):
+            maxh = max(maxh, horizontalCuts[i]-horizontalCuts[i-1])
+        for i in range(1,len(verticalCuts)):
+            maxv = max(maxv, verticalCuts[i]-verticalCuts[i-1])
+            
+        return (maxh*maxv)% (10**9+7)
+        
+            
