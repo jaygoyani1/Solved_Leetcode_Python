5e2a3eb8d8145ac7b60dd501165a8987cf51345c
Jay Goyani <goyanijay@gmail.com>
2021-05-31 10:09:49 -0400

    Time: 220 ms (37.04%), Space: 15.7 MB (25.75%) - LeetHub

--- maximal-square/maximal-square.py
+++ maximal-square/maximal-square.py
@@ -1,23 +1,26 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        dp = [[0]*len(matrix[0]) for i in range(len(matrix))]
-        ans = 0
-         
-        for i in range(len(matrix)):
-            dp[i][0] = int(matrix[i][0])
-            if dp[i][0] == 1:
-                ans = 1
         
-        for j in range(1,len(matrix[0])):
-            dp[0][j] = int(matrix[0][j])
-            if dp[0][j] == 1:
-                ans = 1
+        m = len(matrix)
+        n = len(matrix[0])
         
+        maximal = 0
         
-        for i in range(1,len(matrix)):
-            for j in range(1,len(matrix[0])):
+        for i in range(m):
+            
+            if matrix[i][0] == "1":
+                maximal = 1
+                break
+        for i in range(n):
+            if matrix[0][i] == "1":
+                maximal = 1
+                break
+        
+        for i in range(1,m):
+            for j in range(1,n):
                 if matrix[i][j] == "1":
-                    dp[i][j] = 1 + min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])
-                ans = max(ans, dp[i][j])
-        return ans*ans
-                    
+                    matrix[i][j] = 1 + min(int(matrix[i-1][j-1]), min(int(matrix[i-1][j]), int(matrix[i][j-1])))
+                    maximal = max(maximal, matrix[i][j])
+        return maximal*maximal
+                
+        
