a27add8185792c6f45d8b7d7f67dd14d19e883cd
Jay Goyani <goyanijay@gmail.com>
2021-06-01 10:39:43 -0400

    Time: 64 ms (15.26%), Space: 14.2 MB (64.41%) - LeetHub

--- rotting-oranges/rotting-oranges.py
+++ rotting-oranges/rotting-oranges.py
@@ -1,33 +1,40 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        lenx, leny = len(grid), len(grid[0])
-        q = []
-        fresh= 0
-        for i in range(lenx):
-            for j in range(leny):
-                if grid[i][j] == 2:
-                    q.append([i,j])
-                if grid[i][j]  == 1:
-                    fresh +=1
-        if fresh ==0 and len(q) == 0:
-            return 0
+        lm = len(grid)
+        ln = len(grid[0])
+        
+        fresh = 0
+        queue = []
+        visited = set()
+        for i in range(lm):
+            for j in range(ln):
+                if grid[i][j] == 1:
+                    fresh+=1
+                elif grid[i][j] == 2:
+                    queue.append((i,j))
+
         time = -1
-        dir = [(1,0),(-1,0),(0,1),(0,-1)]
-        while q:
-            n = len(q)
-            time +=1
-            for _ in  range(n):
-                row,col = q.pop(0)
-                for x in dir:
-                    x1 = x[0] + row
-                    y1 = x[1] + col
-                    if x1 < 0 or x1>=lenx or y1<0 or y1>=leny:
-                        continue
-                    if grid[x1][y1] == 1:
-                        grid[x1][y1] = 2
-                        fresh -= 1
-                        q.append([x1,y1])
-        return time if fresh==0 else -1 
+        dir = [(0,1),(0,-1),(1,0),(-1,0)]
+        
+        if fresh == 0:
+            return 0
+        while queue:
+            n= len(queue)
+            time+=1
+            if fresh == 0:
+                return time
+            for _ in range(n):
                 
-                    
-                    
+                a,b = queue.pop(0)
+                for x,y in dir:
+                    p = a + x
+                    q = b + y
+                    if p<0 or p>=lm or q<0 or q>=ln or grid[p][q] !=1:
+                        continue
+                    grid[p][q] = 2
+                    queue.append((p,q))
+                    fresh-=1 
+        return time if fresh == 0 else -1
+
+            
+        
