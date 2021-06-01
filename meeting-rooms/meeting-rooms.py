68c92f35da82c4e2950f0babc5aadf90323d4fa1
Jay Goyani <goyanijay@gmail.com>
2021-06-01 16:24:03 -0400

    Time: 132 ms (5.07%), Space: 17.4 MB (82.23%) - LeetHub

--- meeting-rooms/meeting-rooms.py
+++ meeting-rooms/meeting-rooms.py
@@ -1,8 +1,12 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort()
+        
         for i in range(1,len(intervals)):
-            if (intervals[i-1][1] > intervals[i][0] or intervals[i-1][0] > intervals[i][1]):
+            if intervals[i][0] >= intervals[i-1][1]:
+                continue
+            else:
                 return False
         return True
+                
         
