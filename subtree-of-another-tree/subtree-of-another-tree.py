cf4756f042cf491e65e4aacf7b58ee6497921978
Jay Goyani <goyanijay@gmail.com>
2021-06-01 09:40:26 -0400

    Time: 156 ms (60.08%), Space: 15.4 MB (22.04%) - LeetHub

--- subtree-of-another-tree/subtree-of-another-tree.py
+++ subtree-of-another-tree/subtree-of-another-tree.py
@@ -5,18 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSubtree(self, r: TreeNode, sr: TreeNode) -> bool:
-        def isSame(A,B):
-            if A==None and B==None:
+    def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
+        def same (node1,node2):
+            if not node1 and not node2:
                 return True
-            if A==None or B==None:
+            if not node1 or not node2:
                 return False
-            return A.val==B.val and isSame(A.left,B.left) and isSame(A.right,B.right)
+            return node1.val == node2.val and same(node1.left,node2.left) and same(node1.right,node2.right)
+        
+        if not root and not subRoot:
+            return True
+        if not root or not subRoot:
+            return False
+        return same(root,subRoot) or self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
+        
+    
+    
+    
+    
+    
+#         def isSame(A,B):
+#             if A==None and B==None:
+#                 return True
+#             if A==None or B==None:
+#                 return False
+#             return A.val==B.val and isSame(A.left,B.left) and isSame(A.right,B.right)
     
         
-        if r==None and sr==None:
-            return True
-        if r==None or sr==None:
-            return False
+#         if r==None and sr==None:
+#             return True
+#         if r==None or sr==None:
+#             return False
 
-        return isSame(r,sr) or self.isSubtree(r.left,sr) or self.isSubtree(r.right,sr)
+#         return isSame(r,sr) or self.isSubtree(r.left,sr) or self.isSubtree(r.right,sr)
+        
