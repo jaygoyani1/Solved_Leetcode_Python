4b3df7b12086e1371b0185e0edb393fc8bb9ae14
Jay Goyani <goyanijay@gmail.com>
2021-06-02 09:35:22 -0400

    Time: 32 ms (88.58%), Space: 14.5 MB (34.61%) - LeetHub

--- reorder-data-in-log-files/reorder-data-in-log-files.py
+++ reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -6,13 +6,13 @@ class Solution:
         for log in logs:
             
             split = log.split()
-            if not split[1].isalpha():
+            if split[1].isdigit():
                 digits.append(log)
             else:
                 words.append(log)
         def func(log):
             _id,rest = log.split(" ",maxsplit =1)
-            return (0,rest,_id)            
+            return (rest,_id)            
         words.sort(key = func)
         
         return words + digits
