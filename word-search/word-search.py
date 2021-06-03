5644b2ae4da49e202856ab6c6c638315eebf9085
Jay Goyani <goyanijay@gmail.com>
2021-06-03 11:01:52 -0400

    Time: 5744 ms (15.29%), Space: 14.4 MB (42.13%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,23 +1,34 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def backtrack(i,j,index):
-            if board[i][j] != word[index]:
-                return False
+        lenm = len(board)
+        lenn = len(board[0])
+        
+        direction = [(0,1),(1,0),(0,-1),(-1,0)]
+        def bfs(i,j,index):
             if index == len(word)-1:
                 return True
-            x = board[i][j]
-            board[i][j] = "*"
-            for r,c in [(-1,0),(1,0),(0,-1),(0,1)]:
-                rx = r + i
-                cx = c + j
-                if 0<=rx< len(board) and 0<=cx<len(board[0]):
-                    if backtrack(rx,cx,index+1):
-                        return True
-            board[i][j] = x
-            return False
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if backtrack(i,j,0):
+            
+            temp = board[i][j]
+            board[i][j] = "#"
+            
+            for d in direction:
+                newi = d[0] + i
+                newj = d[1] + j
+                if newi<0 or newi>=lenm or newj<0 or newj>=lenn or board[newi][newj] != word[index+1]:
+                    continue
+                if bfs(newi,newj,index+1):
                     return True
+            board[i][j] = temp
+            return False
+            
+        
+        
+        for i in range(lenm):
+            for j in range(lenn):
+                if board[i][j] == word[0]:
+                    if bfs(i,j,0):
+                        return True
         return False
+                    
+                    
         
