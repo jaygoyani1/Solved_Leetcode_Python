ebe99ae36d914c3937743916dbc44fceab7cf7c6
Jay Goyani <goyanijay@gmail.com>
2021-06-06 11:35:26 -0400

    Time: 48 ms (7.85%), Space: 14.3 MB (54.82%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.py
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,27 +1,36 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-
-        a = list(s.strip(" "))
-        if len(a) == 0:
-            return 0
-        flag = True
         
-        if a[0] == "-":
-            flag = False
+        
         i = 0
-        if a[0] == "-" or a[0] =="+":
-            i = 1
-        st= ""
-        while i < len(a) and a[i].isdigit():
-            st += a[i]
+        negative = False
+        number = 0
+            
+        while i<len(s) and s[i] == " ":
             i+=1
-        if st == "":
-            return 0
-        num = int(st)
-        if flag and num > (2**31-1):
-            return 2**31 - 1
-        if not flag and num > 2**31:
+
+        if i<len(s):
+            if s[i] == "-":
+                negative = True
+                i+=1
+            elif s[i] == "+":
+                i+=1
+
+        while i<len(s):
+            if not s[i].isdigit():
+                break
+            number =number*10 + int(s[i])
+            i+=1
+        
+        number = -number if negative else number
+        
+        if number > (2**31-1):
+            return 2**31-1
+        if number < (-2**31):
             return -2**31
-        if not flag:
-            num = -num
-        return num
+        return number
+                
+
+
+                
+            
