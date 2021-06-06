a0f366007369efbeb3022e59d4b99253e9e1b0aa
Jay Goyani <goyanijay@gmail.com>
2021-06-06 12:43:20 -0400

    Time: 44 ms (34.82%), Space: 14.6 MB (13.63%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,19 +1,20 @@
 class Solution:
-    def permute(self, l: List[int]) -> List[List[int]]:
+    def permute(self, nums: List[int]) -> List[List[int]]:
         
-        ans = []
         
-        def backtrack(comb,used):
-            if len(comb) == len(l):
-                ans.append(comb[:])
+        result = []
+        
+        def backtrack(comb,visited):
+            if len(comb) == len(nums):
+                result.append(comb[:])
                 return
             
-            for i in range(len(l)):
-                if not used[i]:
-                    used[i] = True
-                    comb.append(l[i])
-                    backtrack(comb,used)
-                    used[i] = False
+            for i in range(len(nums)):
+                if not visited[i]:
+                    visited[i] = True
+                    comb.append(nums[i])
+                    backtrack(comb,visited)
+                    visited[i] = False
                     comb.pop()
-        backtrack([],[False]*len(l))
-        return ans
+        backtrack([],[False]*len(nums))
+        return result
