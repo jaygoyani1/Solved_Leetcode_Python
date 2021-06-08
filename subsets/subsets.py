ef73b14f2281e9cb8441418ef21cef0dbeae99cc
Jay Goyani <goyanijay@gmail.com>
2021-06-08 09:32:24 -0400

    Time: 44 ms (11.96%), Space: 14.4 MB (49.00%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,17 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-        ans = []
-        
-        def backtrack(comb,j):
-            if len(comb) == len(nums):
-                ans.append(comb[:])
-                return
-            ans.append(comb[:])
+        def backtrack(first = 0, curr = []):
+            # if the combination is done 
+            output.append(curr[:])
             
-            for i in range(j,len(nums)):
-                comb.append(nums[i])
-                backtrack(comb,i+1)
-                comb.pop()
-        backtrack([],0)
-        return ans
+            for i in range(first, len(nums)):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+                # use next integers to complete the combination
+                backtrack(i + 1, curr)
+                # backtrack
+                curr.pop()
+        
+        output = []
+
+        backtrack()
+        return output
